3f2e0b0a07b1f883552fca095737105caf59111a
Zhuridochka <zhuridochka@gmail.com>
2024-05-18 13:53:16 +0300

    Add files via upload

---
+++ lesson_17/js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Для додавання контенту з айтема №2 в айтем №1 на певному розширенні екрану
+ */
+// document.addEventListener("DOMContentLoaded", function () {
+//   if (window.innerWidth <= 760) {
+//     const item4 = document.querySelector("#grid-item4");
+//     const item5 = document.querySelector("#grid-item5");
+//     item4.setAttribute("data-content", item5.textContent);
+//   }
+//   if (window.innerWidth <= 849) {
+//     const item1 = document.querySelector("#grid-item1");
+//     const item2 = document.querySelector("#grid-item2");
+//     item1.setAttribute("data-content", item2.textContent);
+//   }
+// });
